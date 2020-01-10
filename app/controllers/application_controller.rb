f698738ee3896ad6357e9e2aa45fa4666dc21407
Jeshua Hicks <jhicks7884@outlook.com>
2020-01-10 13:24:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
-
-require_relative '../../config/environment'
-
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -8,44 +5,53 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/posts/new' do
-      erb :new
-    end
+  get '/' do
 
-     post '/posts' do
-      @post = Post.create(params)
-      redirect to '/posts'
-    end
-
-     get '/posts' do
-
-      @posts = Post.all
-
-      erb :index
-
-     end
-
-     get '/posts/:id' do
-      @post = Post.find_by_id(params[:id])
-      erb :show
-     end
-
-     get '/posts/:id/edit' do
-      @post = Post.find_by_id(params[:id])
-      erb :edit
-    end
-
-     patch '/posts/:id' do
-      @post = Post.find_by_id(params[:id])
-      @post.name = params[:name]
-      @post.content = params[:content]
-      @post.save
-      erb :show
-    end
-
-     delete '/posts/:id/delete' do
-      @post = Post.find_by_id(params[:id])
-      @post.delete
-      erb :deleted
-    end
   end
+
+  get '/posts/new' do
+
+    erb :new
+  end
+
+  get '/posts' do
+    @posts = Post.all
+
+    erb :index
+  end
+
+  post '/posts' do
+    @post = Post.create(params[:post])
+
+
+    erb :show
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update(params[:edit_post])
+    @post.save
+
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    erb :delete
+  end
+
+end
