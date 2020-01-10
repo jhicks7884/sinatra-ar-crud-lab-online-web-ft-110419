6d65427f86e371c0b8cd71d42a07ffb05e0503f4
Jeshua Hicks <jhicks7884@outlook.com>
2020-01-10 13:32:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,19 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
-        redirect to '/articles'
-      end
+   get '/' do
+    redirect to '/articles'
+  end
 
-       get '/articles' do
-        @articles = Article.all
-        erb :index
-      end
+   get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
 
-       get '/articles/new' do
-        @article = Article.new
-        erb :new
-      end
+   get '/articles/new' do
+    @article = Article.new
+    erb :new
+  end
 
        post '/articles' do
         @article = Article.create(title: params[:title], content: params[:content])
